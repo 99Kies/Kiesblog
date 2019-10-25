f8926559d37de756ffc7ffefea03034ef7577d30
99Kies <1290017556@qq.com>
2019-10-25 19:02:10 +0800

    update

---
+++ kiesblog/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired,Length
+from flask_ckeditor import CKEditorField
+from kiesblog.models import Category
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(1,20)])
+    password = PasswordField('Password',validators=[DataRequired(),Length(8,128)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign in')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(),Length(1,30)])
+    category = SelectField('Category', coerce=int, default=1)
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField()
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.category.choices = [(category.id, category.name) for category in Category.query.order_by(Category.name).all()]
+    
