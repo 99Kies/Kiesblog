bde1bc00ad8df19a80bf7b93e50dfa08e5270ae1
99Kies <1290017556@qq.com>
2019-10-26 19:36:23 +0800

    forms.py

--- kiesblog/forms.py
+++ kiesblog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
-from wtforms.validators import DataRequired,Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, ValidationError, HiddenField
+from wtforms.validators import DataRequired, Length, Email, URL, Optional
 from flask_ckeditor import CKEditorField
 from kiesblog.models import Category
 
@@ -10,7 +10,9 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign in')
 
+
 class PostForm(FlaskForm):
+    # 使用CKE模块可能会导致xss攻击，需要在后台服务器处理一遍
     title = StringField('Title', validators=[DataRequired(),Length(1,30)])
     category = SelectField('Category', coerce=int, default=1)
     body = CKEditorField('Body', validators=[DataRequired()])
@@ -19,4 +21,24 @@ class PostForm(FlaskForm):
     def __init__(self, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
         self.category.choices = [(category.id, category.name) for category in Category.query.order_by(Category.name).all()]
-    
+
+class CategoryForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(1,30)])
+    submit = SubmitField()
+
+    def validate_name(self, field):
+        if Category.query.filter_by(name=field.data).first():
+            raise ValidationError('Name already in use.')
+
+
+class CommitForm(FlaskForm):
+    author = StringField('Name', validators=[DataRequired(), Length(1,30)])
+    email = StringField('Email' ,validators=[DataRequired(), Email(), Length(1,255)])
+    site = StringField('Site', validators=[Optional(), URL(), Length(0,255)])
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField()
+
+class AdminForm(FlaskForm):
+    author = HiddenField()
+    email = HiddenField()
+    site = HiddenField()
